b2529f321d7187acfe2a6e2255a2a1af3cb719b2
Eyrim <marfxedbot@gmail.com>
2023-09-25 14:59:56 +0100

    Refactored red-4 answers

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -16,101 +16,57 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> validMoves = new ArrayList<>();
-        final int WHITE_STARTING_ROW = 6;
-        final int BLACK_STARTING_ROW = 1;
-        final int TOP_OF_BOARD = 0;
-        final int BOTTOM_OF_BOARD = 7;
-        int bias = this.colour == PlayerColour.WHITE ? -1 : 1;
+        final int direction = this.colour == PlayerColour.WHITE ? -1 : 1;
+        Coordinates oneStep = from.plus(direction, 0);
+        Coordinates twoSteps = from.plus(direction * 2, 0);
 
         // A valid move for a pawn is move up one space OR move down one space (black)
         // Or two spaces if first move
-        if ((from.getRow() + bias >= TOP_OF_BOARD) &&
-                (from.getRow() + bias <= BOTTOM_OF_BOARD)) {
-            if (board.get(from.plus(bias, 0)) == null) {
-                    validMoves.add(
-                            new Move(
-                                    from,
-                                    new Coordinates(
-                                            from.getRow() + bias,
-                                            from.getCol()
-                                    )
-                            )
-                    );
+        if (isWithinBoardBoundaries(from, direction)) {
+            if (!isPieceInSquare(from, direction, board)) {
+                validMoves.add(new Move(from, oneStep));
             }
 
-
             // First turn movement allows two squares, so check two squares in front
-            if (board.get(from.plus((bias * 2), 0)) == null) {
-                if ((from.getRow() == WHITE_STARTING_ROW && this.colour == PlayerColour.WHITE) ||
-                        (from.getRow() == BLACK_STARTING_ROW && this.colour == PlayerColour.BLACK)) {
-                    validMoves.add(
-                            new Move(
-                                    from,
-                                    new Coordinates(
-                                            from.getRow() + (bias * 2),
-                                            from.getCol()
-                                    )
-                            )
-                    );
+            if (!isPieceInSquare(from, direction * 2, board)) {
+                if (isFirstMove(from)) {
+                    validMoves.add(new Move(from, twoSteps));
                 }
             }
         }
-//        if (this.colour == PlayerColour.WHITE) {
-//                if (board.get(from.plus(-1, 0)) == null) {
-//                    coords = new Coordinates(from.getRow() - 1, from.getCol());
-//                    if (!checkIfMoveWillLeaveBoard(coords)) {
-//                        validMoves.add(
-//                                new Move(
-//                                        from,
-//                                        coords
-//                                )
-//                        );
-//                    }
-//
-//                    if (from.getRow() == WHITE_STARTING_ROW) {
-//                        coords = new Coordinates(from.getRow() - 2, from.getCol());
-//                        if (!checkIfMoveWillLeaveBoard(coords)) {
-//                            validMoves.add(
-//                                    new Move(
-//                                            from,
-//                                            coords
-//                                    )
-//                            );
-//                        }
-//                    }
-//                }
-//        } else {
-//            if (board.get(from.plus(1, 0)) == null) {
-//                validMoves.add(
-//                        new Move(
-//                                from,
-//                                new Coordinates(
-//                                        from.getRow() + 1,
-//                                        from.getCol())
-//                        )
-//                );
-//
-//                if (from.getRow() == BLACK_STARTING_ROW) {
-//                    validMoves.add(
-//                            new Move(
-//                                    from,
-//                                    new Coordinates(
-//                                            from.getRow() + 2,
-//                                            from.getCol())
-//                            )
-//                    );
-//                }
-//            }
-//        }
 
         return validMoves;
     }
 
-    private boolean checkIfMoveWillLeaveBoard(Coordinates target) {
-        // Geographical not index based
+    private boolean isWithinBoardBoundaries(Coordinates from, int direction) {
         final int TOP_OF_BOARD = 0;
         final int BOTTOM_OF_BOARD = 7;
 
-        return target.getRow() <= TOP_OF_BOARD || target.getRow() >= BOTTOM_OF_BOARD;
+        if ((from.getRow() + direction >= TOP_OF_BOARD) &&
+                (from.getRow() + direction <= BOTTOM_OF_BOARD)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isPieceInSquare(Coordinates from, int direction, Board board) {
+        if (board.get(from.plus(direction, 0)) != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isFirstMove(Coordinates from) {
+        final int WHITE_STARTING_ROW = 6;
+        final int BLACK_STARTING_ROW = 1;
+
+        if ((from.getRow() == WHITE_STARTING_ROW && this.colour == PlayerColour.WHITE) ||
+                (from.getRow() == BLACK_STARTING_ROW && this.colour == PlayerColour.BLACK)) {
+            return true;
+        }
+
+        return false;
     }
 }
