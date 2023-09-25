82ff2727e4e523d6f8b838e0d6f575c9d1154102
Eyrim <marfxedbot@gmail.com>
2023-09-25 15:38:00 +0100

    Moved Pawn utility methods to Piece interface

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -37,48 +37,12 @@ public class Pawn extends AbstractPiece {
 
             // First turn movement allows two squares, so check two squares in front
             if (!isPieceInSquare(oneStep, board, this.colour) && !isPieceInSquare(twoSteps, board, this.colour)) {
-                if (isFirstMove(from)) {
+                if (isFirstMove(from, this.colour)) {
                     validMoves.add(new Move(from, twoSteps));
                 }
             }
         }
 
         return validMoves;
-    }
-
-    private boolean isWithinBoardBoundaries(Coordinates square) {
-        final int TOP_OF_BOARD = 0; // Row
-        final int BOTTOM_OF_BOARD = 7; // Row
-        final int LEFT_OF_BOARD = 0; // Col
-        final int RIGHT_OF_BOARD = 7; // Col
-
-        if ((square.getRow() >= TOP_OF_BOARD) && (square.getRow() <= BOTTOM_OF_BOARD) &&
-                ((square.getCol() >= LEFT_OF_BOARD) && (square.getCol() <= RIGHT_OF_BOARD))) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean isPieceInSquare(Coordinates square, Board board, PlayerColour colour) {
-        Piece inSquare = board.get(square);
-
-        if (inSquare != null && inSquare.getColour() != colour) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean isFirstMove(Coordinates from) {
-        final int WHITE_STARTING_ROW = 6;
-        final int BLACK_STARTING_ROW = 1;
-
-        if ((from.getRow() == WHITE_STARTING_ROW && this.colour == PlayerColour.WHITE) ||
-                (from.getRow() == BLACK_STARTING_ROW && this.colour == PlayerColour.BLACK)) {
-            return true;
-        }
-
-        return false;
     }
 }
--- app/src/main/java/chessington/model/pieces/Piece.java
+++ app/src/main/java/chessington/model/pieces/Piece.java
@@ -16,4 +16,40 @@ public interface Piece {
     PlayerColour getColour();
 
     List<Move> getAllowedMoves(Coordinates from, Board board);
+
+    public default boolean isWithinBoardBoundaries(Coordinates square) {
+        final int TOP_OF_BOARD = 0; // Row
+        final int BOTTOM_OF_BOARD = 7; // Row
+        final int LEFT_OF_BOARD = 0; // Col
+        final int RIGHT_OF_BOARD = 7; // Col
+
+        if ((square.getRow() >= TOP_OF_BOARD) && (square.getRow() <= BOTTOM_OF_BOARD) &&
+                ((square.getCol() >= LEFT_OF_BOARD) && (square.getCol() <= RIGHT_OF_BOARD))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public default boolean isPieceInSquare(Coordinates square, Board board, PlayerColour colour) {
+        Piece inSquare = board.get(square);
+
+        if (inSquare != null && inSquare.getColour() != colour) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public default boolean isFirstMove(Coordinates from, PlayerColour colour) {
+        final int WHITE_STARTING_ROW = 6;
+        final int BLACK_STARTING_ROW = 1;
+
+        if ((from.getRow() == WHITE_STARTING_ROW && colour == PlayerColour.WHITE) ||
+                (from.getRow() == BLACK_STARTING_ROW && colour == PlayerColour.BLACK)) {
+            return true;
+        }
+
+        return false;
+    }
 }
