e6426ccb93178b54b47fcba858c50b67b609fc85
Eyrim <marfxedbot@gmail.com>
2023-09-25 12:04:08 +0100

    PawnTest passing for two space moves

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -16,21 +16,57 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> validMoves = new ArrayList<>();
-        int bias = this.colour == PlayerColour.WHITE ? -1 : 1;
+        final int WHITE_STARTING_ROW = 6;
+        final int BLACK_STARTING_ROW = 1;
+
 
         // A valid move for a pawn is move up one space OR move down one space (black)
             // This doesn't take into account:
                 // Taking
                 // First move = 2 space allowance
                 // Will allow the piece to go off the board
-        validMoves.add(
-                new Move(
-                        from,
-                        new Coordinates(
-                                from.getRow() + bias,
-                                from.getCol())
-                )
-        );
+        if (this.colour == PlayerColour.WHITE) {
+            validMoves.add(
+                    new Move(
+                            from,
+                            new Coordinates(
+                                    from.getRow() - 1,
+                                    from.getCol())
+                    )
+            );
+
+            if (from.getRow() == WHITE_STARTING_ROW) {
+                validMoves.add(
+                        new Move(
+                                from,
+                                new Coordinates(
+                                        from.getRow() - 2,
+                                        from.getCol()
+                                )
+                        )
+                );
+            }
+        } else {
+            validMoves.add(
+                    new Move(
+                            from,
+                            new Coordinates(
+                                    from.getRow() + 1,
+                                    from.getCol())
+                    )
+            );
+
+            if (from.getRow() == BLACK_STARTING_ROW) {
+                validMoves.add(
+                        new Move(
+                                from,
+                                new Coordinates(
+                                        from.getRow() + 2,
+                                        from.getCol())
+                        )
+                );
+            }
+        }
 
         return validMoves;
     }
